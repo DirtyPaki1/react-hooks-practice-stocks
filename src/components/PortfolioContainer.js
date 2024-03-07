493ea000ac3a35291250a79ac92ce417df79bcf7
Usman <91799557+DirtyPaki1@users.noreply.github.com>
2024-03-07 15:03:39 -0500

    Completed

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,15 +1,15 @@
 import React from "react";
 import Stock from "./Stock";
 
-function PortfolioContainer() {
-  return (
+function PortfolioContainer({ portfolio }) {
+ return (
     <div>
       <h2>My Portfolio</h2>
-      {
-        //render your portfolio stocks here
-      }
+      {portfolio.map((stock) => (
+        <Stock key={stock.id} stock={stock} />
+      ))}
     </div>
-  );
+ );
 }
 
 export default PortfolioContainer;
--- src/components/StockContainer.js
+++ src/components/StockContainer.js
@@ -1,13 +1,15 @@
 import React from "react";
 import Stock from "./Stock";
 
-function StockContainer() {
-  return (
+function StockContainer({ stocks }) {
+ return (
     <div>
       <h2>Stocks</h2>
-      {/* render stock list here*/}
+      {stocks.map((stock) => (
+        <Stock key={stock.id} stock={stock} />
+      ))}
     </div>
-  );
+ );
 }
 
 export default StockContainer;
